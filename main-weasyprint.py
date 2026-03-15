62769d9d2b4c8ffb50387060160f8dbe8d90b45e
Nathan Zimpfer <thoufer@gmail.com>
2026-03-15 08:53:45 -0400

    add docstring and comments to weasyprint build file

--- main-weasyprint.py
+++ main-weasyprint.py
@@ -1,3 +1,11 @@
+"""
+    This build file builds a PDF document from an HTML and CSS styling using weasyprint.
+
+    NOTE: weasyprint has an external dependency for the Pango library maintained by GTK.
+    Installing on Windows may be a chore, given that GTK is linux os based.  So,
+    recommendation is to run this via MacOS, Docker, or possibly WSL2, but I haven't
+    tested on anything other than MacOS.
+"""
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
 from weasyprint import HTML, CSS
@@ -17,13 +25,17 @@ def main(file: str, build_target: str) -> None:
         target_path.mkdir(parents=True, exist_ok=True)
         target_file = target_path / f'Statement-{project.id}'
 
-        # render directly from string
+        # This render the jinja template the HTML directly to a string which is stored in
+        # python, and then that string is passed to weasyprint to render the final PDF.
         rendered_html = template.render(project=project)
         document = HTML(string=rendered_html).render(
             stylesheets=[CSS('static/css/soi-template.css')])
         document.write_pdf(target_file.with_suffix('.pdf'))
 
-        # render from intermediate html file
+        # This code block renders the jinja template into an HTML file stored locally.
+        # That HTML file is then passed to weasyprint to render the final PDF.  NOTE
+        # we can render the HTML and PDF using different underlying CSS layouts,
+        # potentially making this more flexible in a production setting.
         # html_file = open(target_file.with_suffix('.html'), 'w')
         # html_file.write(rendered_html)
         # html_file.close()
